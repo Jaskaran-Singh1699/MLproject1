535f1153526ac315b45f1469800fd7867dc2cdf9
Jaskaran-Singh1699 <jaskaransaggu1699@gmail.com>
2024-09-11 23:22:29 +0530

    Hyperparameter Training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -41,17 +41,47 @@ class ModelTrainer:
 
             models={
                 "Random Forest":RandomForestRegressor(),
-                "LinearRegression":LinearRegression(),
+                "Linear Regression":LinearRegression(),
                 "Decision Tree":DecisionTreeRegressor(),
                 "Gradient Boosting":GradientBoostingRegressor(),
-                "K-Neighbors Classifiers":KNeighborsRegressor(),
-                "XGBClassifier":XGBRegressor(),
-                "CatBoost Classifier":CatBoostRegressor(verbose=False),
-                "AdaBoost Classifier":AdaBoostRegressor(),
+                # "K-Neighbors Classifiers":KNeighborsRegressor(),
+                "XGBRegressor":XGBRegressor(),
+                "CatBoost Regressor":CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor":AdaBoostRegressor(),
             }
 
 
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            # giving hyperparameters
+            params={
+                "Decision Tree":{
+                    'criterion':['squared_error','friedman_mse','absolute_error','poisson']
+                },
+                "Random Forest":{
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    'n_estimators':[8,16,32,64,128,256],
+                    'learning_rate':[.1,.01,.001,0.05],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[0.1,0.01,0.05,0.001]
+                },
+                "CatBoost Regressor":{
+                    'depth':[6,8,10],
+                    'learning_rate':[0.01,0.05,0.1],
+                    'iterations':[30,50,100]
+                },
+                "AdaBoost Regressor":{
+                    'n_estimators':[8,16,32,64,128,256],
+                    'learning_rate':[0.1,0.01,0.5,0.001]
+                }
+            }
+
+
+
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param=params)
 
             # get best score ffrom dict
             best_model_score=max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import pandas as pd
 import dill
 
 from src.exception import Custom_Exception
-
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import r2_score
 def save_object(file_path,obj):
     try:
@@ -18,13 +18,23 @@ def save_object(file_path,obj):
     except Exception as e: 
         raise Custom_Exception(e,sys)
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         report={}
 
         for i in range(len(list(models))):
             model=list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
+
+
+
+            # model.fit(X_train,y_train)
             y_train_pred=model.predict(X_train)
             y_test_pred=model.predict(X_test)
 
